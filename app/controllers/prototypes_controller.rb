7224d58a6b64bc67d300ccfc6e22c99c7c311129
shiromachi <mustard.dreieck@gmail.com>
2020-10-06 22:57:15 +0900

    いったん完成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -46,5 +49,11 @@ class PrototypesController < ApplicationController
   
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
